ffc8d034412268abe9fac5dc583a035d854cb4cc
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2022-02-18 09:34:00 +0100

    UDP client-server

---
+++ src/Client.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+    private DatagramSocket datagramSocket;
+    private InetAddress inetAddress;
+    private byte[] buffer;
+
+    public Client(DatagramSocket datagramSocket, InetAddress inetAddress) {
+        this.datagramSocket = datagramSocket;
+        this.inetAddress = inetAddress;
+    }
+
+    public void sendThenReceive(){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            try{
+                String messageToSend = scanner.nextLine();
+                buffer = messageToSend.getBytes();
+                DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length, inetAddress, 1234);
+                datagramSocket.send(datagramPacket);
+                datagramSocket.receive(datagramPacket);
+                String messageFromServer = new String(datagramPacket.getData(),0,datagramPacket.getLength());
+                System.out.println("The server says you said: "+ messageFromServer);
+            }catch (IOException e){
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SocketException, UnknownHostException {
+        DatagramSocket datagramSocket = new DatagramSocket();
+        InetAddress inetAddress = InetAddress.getByName("localhost");
+        Client client = new Client(datagramSocket,inetAddress);
+        System.out.println("Send datagram packets to a server.");
+        client.sendThenReceive();
+    }
+}
